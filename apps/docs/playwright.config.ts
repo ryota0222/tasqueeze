cee60d05db76fc718e7ed232dfa1b8048e6b9bef
Matsumoto Ryota <awpjdmga1943@icloud.com>
2024-07-19 20:09:05 +0900

    feat: Add npm command for test

--- apps/docs/playwright.config.ts
+++ apps/docs/playwright.config.ts
@@ -6,7 +6,19 @@ const config: PlaywrightTestConfig = {
 		port: 4173
 	},
 	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+	use: {
+		// スクリーンショットの保存を有効にします
+		screenshot: 'only-on-failure', // 'on', 'off', 'only-on-failure' から選択
+		// ビデオの保存を有効にします
+		video: 'retain-on-failure', // 'on', 'off', 'retain-on-failure' から選択
+		// トレースの保存を有効にします
+		trace: 'on-first-retry' // 'on', 'off', 'retain-on-failure', 'on-first-retry' から選択
+	},
+	reporter: [
+		['list'], // デフォルトのレポーター
+		['html', { open: 'never' }] // HTMLレポートを生成
+	]
 };
 
 export default config;
