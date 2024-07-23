fc94ac853ee29f02addb365a7b951b2d075389df
Matsumoto Ryota <awpjdmga1943@icloud.com>
2024-07-23 12:19:44 +0900

    feat: Add web

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,7 +1,35 @@
 <script lang="ts">
 	import { MyCounterButton } from '@repo/ui';
+	import type { TaskManagerClient } from '@repo/proto/generate/web/ServiceServiceClientPb';
+	import { onMount } from 'svelte';
+
+	let client: TaskManagerClient | null = null;
+	
+	
+	onMount(async () => {
+		if (typeof window !== 'undefined') {
+			const { TaskManagerClient } = await import('@repo/proto/generate/web/ServiceServiceClientPb');
+			client = new TaskManagerClient('http://localhost:8080');
+		}
+	})
+
+
+	const getTodoList = async () => {
+		const todoService = await import('@repo/proto/generate/web/service_pb');
+		if (client) {
+		const request = new todoService.default.TodosRequest();
+		request.setPage(1);
+		request.setLimit(10);
+		
+		const result = await client.getTodoList(request);
+		console.log(result);
+		}
+	}
+
 </script>
 
 <h1>Web</h1>
 <MyCounterButton />
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={getTodoList}>Get Todos</button>
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,14 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import commonjs from 'vite-plugin-commonjs';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), commonjs(), nodePolyfills()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
 	build: {
 		commonjsOptions: {
-			include: [/@repo\/ui/, /node_modules/]
+			include: [/@repo\/ui/, /@repo\/proto/, /node_modules/]
 		}
 	}
+	// ssr: {
+	// 	noExternal: ['google-protobuf'] // Add google-protobuf to noExternal list
+	// },
+	// define: {
+	// 	self: 'globalThis' // or use 'self': 'this'
+	// }
 });
